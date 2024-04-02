7184f2b8bfd49e68236ae3ccfb38947ead75d098
Xavier GARNAUD <garnaudxavier@gmail.com>
2024-04-02 17:21:29 +0200

    initial commit

---
+++ kahip-sys/build.rs
@@ -0,0 +1,85 @@
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    let include_dir = match env::var("KAHIP_INCLUDE_DIR") {
+        Ok(x) => PathBuf::from(x),
+        Err(_) => match env::var("KAHIP_DIR") {
+            Ok(x) => PathBuf::from(x).join("include"),
+            Err(_) => PathBuf::from("/usr/include"),
+        },
+    };
+    let kahip_h = include_dir
+        .join("kaHIP_interface.h")
+        .into_os_string()
+        .into_string()
+        .expect("Not an UTF-8 path");
+    let lib_dir = match env::var("KAHIP_LIB_DIR") {
+        Ok(x) => Some(PathBuf::from(x)),
+        Err(_) => env::var("KAHIP_DIR")
+            .map(|x| PathBuf::from(x).join("lib"))
+            .ok(),
+    };
+    if let Some(lib_dir) = lib_dir {
+        let lib_dir = lib_dir
+            .into_os_string()
+            .into_string()
+            .expect("Not an UTF-8 path");
+        println!("cargo:rustc-link-search={}", lib_dir);
+        #[cfg(target_os = "macos")]
+
+        println!("cargo:rustc-link-arg=-Wl,-rpath,{}", lib_dir);
+        #[cfg(target_os = "linux")]
+        println!("cargo:rustc-link-arg=-Wl,-rpath,{}", lib_dir);
+        // non standard key
+        // see https://doc.rust-lang.org/cargo/reference/build-script-examples.html#linking-to-system-libraries
+        // and https://github.com/rust-lang/cargo/issues/5077
+        println!("cargo:rpath={}", lib_dir);
+    }
+    println!("cargo:rerun-if-changed={}", kahip_h);
+    println!("cargo:rustc-link-lib=kahip");
+
+    bindgen::Builder::default()
+        .header("stdbool.h")
+        .header(kahip_h)
+        .allowlist_function("kaffpa.*")
+        .allowlist_function("process_mapping")
+        .allowlist_function("node_separator")
+        .allowlist_function("reduced_nd")
+        .allowlist_function("edge_partitioning")
+        .allowlist_var("FAST.*")
+        .allowlist_var("ECO.*")
+        .allowlist_var("STRONG.*")
+        .allowlist_var("MAPMODE_.*")
+        .generate()
+        .expect("Unable to generate bindings")
+        .write_to_file("src/binding.rs")
+        .expect("Couldn't write bindings!");
+
+    // let bindings = bindgen::builder()
+    //     .header("wrapper.h")
+    //     .allowlist_function("kaffpa.*")
+    //     .allowlist_function("process_mapping")
+    //     .allowlist_function("node_separator")
+    //     .allowlist_function("reduced_nd")
+    //     .allowlist_function("edge_partitioning")
+    //     .allowlist_var("FAST.*")
+    //     .allowlist_var("ECO.*")
+    //     .allowlist_var("STRONG.*")
+    //     .allowlist_var("MAPMODE_.*")
+    //     .generate()
+    //     .unwrap_or_else(|err| {
+    //         eprintln!("Failed to generate bindings to KaHIP: {err}");
+    //         process::exit(1);
+    //     });
+
+    // let out_path = PathBuf::from(env::var("OUT_DIR").unwrap()).join("bindings.rs");
+    // bindings.write_to_file(&out_path).unwrap_or_else(|err| {
+    //     eprintln!(
+    //         "Failed to write KaHIP bindings to {:?}: {}",
+    //         out_path.display(),
+    //         err,
+    //     );
+    //     process::exit(1);
+    // });
+}
---
+++ src/lib.rs
@@ -0,0 +1,157 @@
+//! This crate provides a thin but idiomatic API around KaHIP.
+
+use core::mem;
+use core::ptr;
+use kahip_sys as m;
+
+pub enum Mode {
+    Fast = m::FAST as isize,
+    Eco = m::ECO as isize,
+    Strong = m::STRONG as isize,
+    FastSocial = m::FASTSOCIAL as isize,
+    EcoSocial = m::ECOSOCIAL as isize,
+    StrongSocial = m::STRONGSOCIAL as isize,
+}
+
+pub type Idx = std::os::raw::c_int;
+
+/// Builder structure to setup a graph partition computation.
+///
+/// This structure holds the required arguments for KaHIP to compute a
+/// partition. It also offers methods to easily set any optional argument.
+///
+
+#[derive(Debug, PartialEq)]
+pub struct Graph<'a> {
+    /// The adjency structure of the graph (part 1).
+    xadj: &'a mut [Idx],
+
+    /// The adjency structure of the graph (part 2).
+    ///
+    /// Required size: xadj.last()
+    adjncy: &'a mut [Idx],
+
+    /// The computational weights of the vertices.
+    ///
+    /// Required size: ncon * (xadj.len()-1)
+    vwgt: Option<&'a mut [Idx]>,
+
+    /// The weight of the edges.
+    ///
+    /// Required size: xadj.last()
+    adjwgt: Option<&'a mut [Idx]>,
+}
+
+impl<'a> Graph<'a> {
+    /// Creates a new [`Graph`] object to be partitioned.
+    ///
+    /// # Panics
+    ///
+    /// This function panics if:
+    /// - any of the arrays have a length that cannot be hold by an [`Idx`], or
+    /// - `ncon` is not strictly greater than zero, or
+    /// - `nparts` is not strictly greater than zero, or
+    /// - `xadj` is empty, or
+    /// - the length of `adjncy` is different than the last element of `xadj`.
+    ///
+    /// # Mutability
+    ///
+    /// While nothing should be modified by the [`Graph`] structure, KaHIP
+    /// doesn't specify any `const` modifier, so everything must be mutable on
+    /// Rust's side.
+    pub fn new(xadj: &'a mut [Idx], adjncy: &'a mut [Idx]) -> Graph<'a> {
+        assert_ne!(xadj.len(), 0);
+        assert_eq!(adjncy.len(), *xadj.last().unwrap() as usize);
+
+        Graph {
+            xadj,
+            adjncy,
+            adjwgt: None,
+            vwgt: None,
+        }
+    }
+
+    /// Sets the computational weights of the vertices.
+    ///
+    /// By default all vertices have the same weight.
+    pub fn set_vwgt(mut self, vwgt: &'a mut [Idx]) -> Graph<'a> {
+        assert_eq!(vwgt.len(), self.xadj.len() - 1);
+        self.vwgt = Some(vwgt);
+        self
+    }
+
+    /// Sets the weights of the edges.
+    ///
+    /// By default all edges have the same weight.
+    pub fn set_adjwgt(mut self, adjwgt: &'a mut [Idx]) -> Graph<'a> {
+        assert_eq!(
+            adjwgt.len(),
+            (*self.xadj.last().unwrap()).try_into().unwrap()
+        );
+        self.adjwgt = Some(adjwgt);
+        self
+    }
+
+    /// Partition the graph
+    pub fn partition(
+        &mut self,
+        n_parts: Idx,
+        imbalance: f64,
+        suppress_output: bool,
+        seed: Idx,
+        mode: Mode,
+    ) -> (Vec<Idx>, Idx) {
+        let nvtxs = &mut (self.xadj.len() as Idx - 1) as *mut Idx;
+        let xadj = self.xadj.as_mut_ptr();
+        let adjncy = self.adjncy.as_mut_ptr();
+        let vwgt = if let Some(vwgt) = self.vwgt.as_mut() {
+            vwgt.as_mut_ptr()
+        } else {
+            ptr::null_mut()
+        };
+        let adjwgt = if let Some(adjwgt) = self.adjwgt.as_mut() {
+            adjwgt.as_mut_ptr()
+        } else {
+            ptr::null_mut()
+        };
+
+        let mut edgecut = mem::MaybeUninit::uninit();
+        let mut part = vec![0; self.xadj.len() - 1];
+
+        let mut n_parts = n_parts;
+        let mut imbalance = imbalance;
+
+        unsafe {
+            m::kaffpa(
+                nvtxs,
+                vwgt,
+                xadj,
+                adjwgt,
+                adjncy,
+                &mut n_parts as *mut Idx,
+                &mut imbalance as *mut f64,
+                suppress_output,
+                seed,
+                mode as Idx,
+                edgecut.as_mut_ptr(),
+                part.as_mut_ptr(),
+            );
+            (part, edgecut.assume_init())
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use crate::{Graph, Mode};
+    #[test]
+    fn test() {
+        let mut xadj = vec![0, 1, 2];
+        let mut adjncy = vec![1, 0];
+
+        let (part, edgcut) =
+            Graph::new(&mut xadj, &mut adjncy).partition(2, 1e-3, false, 1234, Mode::Fast);
+        println!("{:?} {:?}", part, edgcut);
+    }
+}
